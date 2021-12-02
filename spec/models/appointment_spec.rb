46036b4faf94980cf98360386f70cbb73e7fee71
Morenikeji Bello <caeserselassie@gmail.com>
2021-12-02 16:48:49 +0100

    Write validation and association tests for Appointment model

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:doctor) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:time) }
 end
