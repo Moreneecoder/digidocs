91e115072a1fc0d362edde664212f7dcb407c603
Morenikeji Bello <caeserselassie@gmail.com>
2021-12-06 15:44:35 +0100

    Write test for /users/:id GET request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,6 +14,7 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     render json: {
+      id: @patient.id,
       name: @patient.name,
       phone: @patient.phone,
       email: @patient.email
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pp'
 
 RSpec.describe 'Users', type: :request do
   let!(:users) { create_list(:user, 10) }
@@ -15,6 +16,35 @@ RSpec.describe 'Users', type: :request do
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /todos/:id
+  describe 'GET /users/:id' do
+    before { get "/api/v1/users/#{user_id}" }
+
+    context 'when the record exists' do
+      it 'returns the user with the given id' do
+        pp user_id
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(users.first.id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:user_id) { 100 }
+
+      it 'returns status code 404' do        
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find User/)
+      end
     end
   end
 end
