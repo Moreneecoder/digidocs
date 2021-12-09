bcfe96d5370110d887feb235ed1b23ea2af9335e
Morenikeji Bello <caeserselassie@gmail.com>
2021-12-09 16:11:48 +0100

    Parse request body to JSON

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -34,7 +34,8 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :phone, :email)
+    parsed_params = JSON.parse(params)
+    parsed_params.permit(:name, :phone, :email)
   end
 
   def set_user
