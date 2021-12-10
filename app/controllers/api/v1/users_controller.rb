018f2d709c56fb1f86bbab336299e14cdfa07db0
Morenikeji Bello <caeserselassie@gmail.com>
2021-12-10 18:17:57 +0100

    Fix double rendering error in login action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,8 +29,11 @@ class Api::V1::UsersController < ApplicationController
 
   def login
     @user = User.where(name: params[:name], email: params[:email]).first
-    render json: { status: 200, user: @user }, status: :ok if @user
-    render json: {status: 404, message: 'User not found'}, status: :not_found
+    if @user
+      render json: { status: 200, user: @user }, status: :ok
+    else
+      render json: {status: 404, message: 'User not found'}, status: :not_found
+    end
   end
 
   def destroy
